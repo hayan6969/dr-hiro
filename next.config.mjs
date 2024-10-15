cd46e57da3747df49d91eb0f62b11b115cf81a1a
Hayan Khan <hayankhanmehsood@gmail.com>
2024-10-15 12:29:33 +0500

    unsplash images fixed

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
     eslint: {
       ignoreDuringBuilds: true, //disabled es lint
+      
     },
+    images: {
+        domains: ["images.unsplash.com", "cdn.pixabay.com", "images.pexel.com"],
+      },
   };
   
   export default nextConfig;
